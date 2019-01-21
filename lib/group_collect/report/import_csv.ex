cdfcdcbd8fbf51fd46e68c0cdb782e0fc151f256
André Souza <andrestephano@gmail.com>
2019-01-21 20:46:44 -0200

    adds a working parser to the csv

--- lib/group_collect/report/import_csv.ex
+++ lib/group_collect/report/import_csv.ex
@@ -1,6 +1,14 @@
-NimbleCSV.define(CommaCSVParser, separator: ",", escape: "\"")
-
 defmodule GroupCollect.Report.ImportCSV do
+  @moduledoc """
+  Parse the GroupCollect CSV containing the status of many passengers.
+
+  The CSV should be formated as RFC4180 standards and should have the following columns:
+
+  Passenger ID | Full Name | Gender | Email | Package | Date of Birth | Status
+  """
+
+  alias NimbleCSV.RFC4180, as: CSV
+
   @header_keys [
     "Passenger ID",
     "Full Name",
@@ -10,8 +18,24 @@ defmodule GroupCollect.Report.ImportCSV do
     "Date of Birth",
     "Status"
   ]
+
+  @type csv_content :: binary()
+  @type t :: %{
+          "Passenger ID": String.t(),
+          "Full Name": String.t(),
+          Gender: String.t(),
+          Email: String.t(),
+          Package: String.t(),
+          "Date of Birth": String.t(),
+          Status: String.t()
+        }
+
+  @doc """
+  Verifies if the csv are well formated and contains all the required columns
+  """
+  @spec validate_header(csv_content()) :: boolean()
   def validate_header(string) do
-    [header | _] = CommaCSVParser.parse_string(string, headers: false)
+    [header | _] = parse_raw(string)
 
     case validate_header_keys(header) do
       true -> true
@@ -19,24 +43,31 @@ defmodule GroupCollect.Report.ImportCSV do
     end
   end
 
-  # def parse(string) do
-  #   case validate_header(string) do
-  #     false -> {:error, :invalid_headers}
-  #     true ->
-  #       string
-  #       |> CommaCSVParser.parse_string(headers: false)
-
-  #   end
-  # end
-
+  @doc """
+  Receives the csv string and tries to parse into a map
+  """
+  @spec parse(csv_content()) ::
+          {:error, %{msg: binary(), required: [binary(), ...]}} | {:ok, [t(), ...]}
   def parse(string) do
+    case validate_header(string) do
+      true -> {:ok, parse_valid(string)}
+      false -> {:error, %{msg: "missing_required_keys", required: @header_keys}}
+    end
+  end
+
+  defp parse_raw(string) do
+    CSV.parse_string(string, headers: false)
+  end
+
+  defp parse_valid(string) do
     [header | body] =
       string
-      |> CommaCSVParser.parse_string(headers: false)
+      |> parse_raw
 
     body
     |> Enum.map(fn row ->
-      Enum.zip(header, row)
+      header
+      |> Enum.zip(row)
       |> Enum.into(%{})
     end)
   end
--- test/group_collect/report/import_csv_test.exs
+++ test/group_collect/report/import_csv_test.exs
@@ -9,15 +9,15 @@ defmodule GroupCollect.Report.ImportCSVTest do
   @tabulated_csv "Passenger ID\tFull Name\tGender\tEmail\tPackage\tDate of Birth\tStatus"
 
   describe "CommaCSV" do
-    test "validate_header/1 should return true for right values" do
+    test "validate_header/1 should return true when csv have all the valid values" do
       assert true == ImportCSV.validate_header(@valid_comma_csv)
     end
 
-    test "validate_header/1 should return false for wrong header keys" do
+    test "validate_header/1 should return false for missing keys" do
       assert false == ImportCSV.validate_header(@invalid_comma_csv)
     end
 
-    test "parse/1 should convert to map no matter the order" do
+    test "parse_valid/1 should convert the csv a map no matter the order" do
       right_map = [
         %{
           "Date of Birth" => "2006-02-14",
@@ -30,8 +30,10 @@ defmodule GroupCollect.Report.ImportCSVTest do
         }
       ]
 
-      assert right_map == ImportCSV.parse(@valid_comma_csv)
-      assert right_map == ImportCSV.parse(@valid_out_or_order_comma_csv)
+      assert {:ok, right_map} == ImportCSV.parse(@valid_comma_csv)
+      assert {:ok, right_map} == ImportCSV.parse(@valid_out_or_order_comma_csv)
+      assert {:error, _} = ImportCSV.parse(@invalid_comma_csv)
+      assert {:error, _} = ImportCSV.parse(@tabulated_csv)
     end
   end
 end
