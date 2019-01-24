a10723a6b676f863323a4e9d321a4a0d5937c886
André Souza <andrestephano@gmail.com>
2019-01-24 01:05:30 -0200

    adds filter

---
+++ lib/group_collect/filter.ex
@@ -0,0 +1,46 @@
+defmodule GroupCollect.Report.Filter do
+  @moduledoc """
+  API for filtering the report list
+  """
+
+  import Ecto.Query
+
+  alias GroupCollect.Report.ReportRowView
+
+  def filter(params) do
+    params
+    |> reduce_apply_filters(ReportRowView)
+  end
+
+  defp reduce_apply_filters(params, query) do
+    params
+    |> Map.to_list()
+    |> Enum.reduce(query, fn x, query_acc ->
+      apply_filter(query_acc, x)
+    end)
+  end
+
+  defp apply_filter(query, {:gender, gender}) do
+    query |> where([p], p.gender == ^gender)
+  end
+
+  defp apply_filter(query, {:package, gender}) do
+    query |> where([p], p.package == ^gender)
+  end
+
+  defp apply_filter(query, {:status, status}) do
+    query |> where([p], p.status == ^status)
+  end
+
+  defp apply_filter(query, {:age, "above 18"}) do
+    start_date = Timex.now() |> Timex.shift(years: -18)
+    query |> where([p], p.birthday <= ^start_date)
+  end
+
+  defp apply_filter(query, {:age, "under 18"}) do
+    start_date = Timex.now() |> Timex.shift(years: -18)
+    query |> where([p], p.birthday > ^start_date)
+  end
+
+  defp apply_filter(query, {:age, _}), do: query
+end
