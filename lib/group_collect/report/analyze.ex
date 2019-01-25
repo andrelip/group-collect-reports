8423cb440869c6893f489a452bd916664b6de70c
André Souza <andrestephano@gmail.com>
2019-01-25 02:16:23 -0200

    creates GroupCollect.Analyze to compile pre-made reports

---
+++ lib/group_collect/report/analyze.ex
@@ -0,0 +1,41 @@
+defmodule GroupCollect.Analyze do
+  import Ecto.Query
+
+  alias GroupCollect.Repo
+  alias GroupCollect.Report.ReportRowView
+
+  def summarize_by_statuses do
+    from(r in ReportRowView,
+      group_by: r.status,
+      select: %{
+        r.status => count(r.status)
+      }
+    )
+    |> Repo.all()
+  end
+
+  def summarize_by_package_for_paid_passengers(wizard: wizard) do
+    case wizard do
+      false ->
+        from(r in ReportRowView,
+          where: r.status == "Fully Paid" or r.status == "Partially Paid",
+          group_by: r.package,
+          select: %{
+            r.package => count(r.package)
+          }
+        )
+
+      true ->
+        from(r in ReportRowView,
+          where:
+            r.status == "Fully Paid" or r.status == "Partially Paid" or
+              r.status == "Finished Wizard",
+          group_by: r.package,
+          select: %{
+            r.package => count(r.package)
+          }
+        )
+    end
+    |> Repo.all()
+  end
+end
