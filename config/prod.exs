069134181643dd7abdc43f3e2d4ac158d984d3ef
André Souza <andrestephano@gmail.com>
2019-01-26 02:51:39 -0200

    ready to deploy

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,9 @@ use Mix.Config
 config :group_collect, GroupCollectWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  root: "."
 
 # Do not print debug messages in production
 config :logger, level: :info
---
+++ docker/scripts/start-server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+set -e
+./bin/group_collect foreground
